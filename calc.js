0cc74081ac2fc84b0923ac8d0d8cedeef09a164b
Pim <pim@vicompany.nl>
2016-11-22 08:40:59 +0100

    added slope function

--- calc.js
+++ calc.js
@@ -1,5 +1,6 @@
 const Calc = {
 	distanceBetween: (x1, y1, x2, y2) => Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)),
+	slope: (x1, y1, x2, y2) => (y2 - y1) / (x2 - x1),
 	angleBetween: (x1, y1, x2, y2) => Math.atan2(y2 - y1, x2 - x1),
 	angleDifference: (angleStart, angleTarget) => Math.atan2(Math.sin(angleTarget - angleStart), Math.cos(angleTarget - angleStart)),
 	clamp: (value, min, max) => Math.max(min, Math.min(value, max)),
