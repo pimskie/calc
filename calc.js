a5b61b0e472f4c5de3f5641bad8b90873f9f50a5
Pim <pim@vicompany.nl>
2017-01-30 08:39:47 +0100

    round optional

--- calc.js
+++ calc.js
@@ -4,5 +4,9 @@ const Calc = {
 	angleBetween: (x1, y1, x2, y2) => Math.atan2(y2 - y1, x2 - x1),
 	angleDifference: (angleStart, angleTarget) => Math.atan2(Math.sin(angleTarget - angleStart), Math.cos(angleTarget - angleStart)),
 	clamp: (value, min, max) => Math.max(min, Math.min(value, max)),
-	randomBetween: (min, max) => ~~(Math.random() * (max - min + 1)) + min
+	randomBetween: (min, max, round = true) => {
+		const rand = (Math.random() * (max - min + 1)) + min;
+
+		return (round) ? ~~rand : rand;
+	}
 }
